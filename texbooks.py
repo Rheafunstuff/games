f96c15bb387a16d9cf1dfc77e26a40cb03d19757
Rheafunstuff <rheamehra145@gmail.com>
2026-06-20 11:13:19 +0100

    Add files via upload

---
+++ texbooks.py
@@ -0,0 +1,9 @@
+dictionary={"math textbook":300}
+print(dictionary)
+dictionary["math textbook"]=200
+print (dictionary)
+dictionary["english texbook"]=250
+dictionary["science textbook"]=400
+print(dictionary)
+operation=input("choose a textbook ")
+print(dictionary[operation])

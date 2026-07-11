277d3be939b1c0264637f2887f57fea9f9b346d4
Rheafunstuff <rheamehra145@gmail.com>
2026-07-11 11:21:11 +0100

    Add files via upload

---
+++ recordingdata.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    operation=input("Enter the group's name ")
+    question=input("Enter the size of the group ")
+    date=input("Enter the date of the competition ")
+    venue=input("Enter the place where the competition took place ")
+    medal=input("Enter the type of medal that the was awarded ")
+    tuple=(operation, question, date, venue, medal)
+    print(tuple)
+
